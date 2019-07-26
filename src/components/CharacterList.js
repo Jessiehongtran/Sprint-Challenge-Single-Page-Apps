48eb7217c06270e13d366041edfee57570010d6d
Your Name <you@example.com>
2019-07-26 12:06:56 -0400

    fetched data from Axios

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,12 +1,27 @@
 import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
+
+
+
+
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
+  const [character, setCharacter] = useState([]);
 
   useEffect(() => {
     // TODO: Add AJAX/API Request here - must run in `useEffect`
+    Axios
+        .get(`https://rickandmortyapi.com/api/character/`)
+        .then(response => {
+          console.log('response', response.data.results)
+          setCharacter(response.data.results)
+        })
+        .catch(err=>{
+          console.log('err', err)
+        })
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [])
+  }, [character])
 
   return <section className='character-list grid-view'>
 
