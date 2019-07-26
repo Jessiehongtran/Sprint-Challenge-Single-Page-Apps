1ecfaeb52002dc4f56c1619f2816f0fb9943cd80
Your Name <you@example.com>
2019-07-26 13:20:43 -0400

    got all locations displayed but at the wrong route

--- src/components/LocationCard.js
+++ src/components/LocationCard.js
@@ -1,7 +1,33 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
+import Axios from 'axios';
 
-export default function LocationCard ({ name, type, dimension, residents }) {
-  // image={image}
-  return (<span>todo: location</span>
+export default function LocationCard (props) {
+  console.log('props in Location List', props.data)
+  const [eachLocation, setEachLocation] = useState({});
+
+  useEffect(()=>{
+    const id = props.data.id
+    console.log('id in LocationList', id)
+
+    Axios
+        .get(`https://rickandmortyapi.com/api/location/${id}`)
+        .then(response =>{
+          console.log('response in LocationList', response.data)
+          setEachLocation(response.data)
+        })
+        .catch(err => {
+          console.log('err in LocationList', err)
+        })
+  
+  }, [props.data.id])
+
+
+  return (
+  <span>
+    <p>{eachLocation.name}</p>
+    <p>{eachLocation.type}</p>
+    <p>{eachLocation.dimension}</p>
+    <p>{eachLocation.resident}</p>
+  </span>
   )
 }
