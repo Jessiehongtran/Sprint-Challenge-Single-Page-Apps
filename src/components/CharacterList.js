3e601929f2aaf9dda6caaaa7bd585c2c339c17d3
Your Name <you@example.com>
2019-07-26 12:25:25 -0400

    mapped over data to link to API of each character:

--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
+import {Link} from 'react-router-dom'
 
 
 
@@ -14,18 +15,24 @@ export default function CharacterList() {
     Axios
         .get(`https://rickandmortyapi.com/api/character/`)
         .then(response => {
-          console.log('response', response.data.results)
+          // console.log('response', response.data.results)
           setCharacter(response.data.results)
         })
         .catch(err=>{
           console.log('err', err)
         })
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-  }, [character])
+  }, [])
 
   return <section className='character-list grid-view'>
 
-      <h2>TODO: `array.map()` over your state here!</h2>
+      <h2>TODO: `array.map()` over your state here!
+        {character.map((data,i) =>(
+          // console.log('data', data)
+          <Link to ={`/character/${data.id}`} key={i}/>
+          
+        ))}
+      </h2>
     </section>
 
 }
