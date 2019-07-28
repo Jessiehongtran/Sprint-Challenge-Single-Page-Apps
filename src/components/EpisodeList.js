a22967758592ed9a403d18067a0eb514c466523b
Your Name <you@example.com>
2019-07-28 13:12:08 -0400

    figured out a typo while importing EpisodeCard from CharacterCard

--- src/components/EpisodeList.js
+++ src/components/EpisodeList.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import Axios from 'axios';
-import EpisodeCard from "./CharacterCard"
+import EpisodeCard from "./EpisodeCard"
 
 
 export default function EpisodeList() {
