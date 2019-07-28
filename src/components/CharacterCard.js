3720bead5a4480d41a2575af6c4a3480865b7465
Your Name <you@example.com>
2019-07-28 12:37:26 -0400

    deleted useState and useEffect in Character Cards because they are not neccesary

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import Axios from 'axios';
 import LocationCard from "./LocationCard"
 import EpisodeCard from "./EpisodeCard"
@@ -7,30 +7,14 @@ import {Card, Image, Icon} from "semantic-ui-react"
 
 export default function CharacterCard (props) {
   console.log('props in CharacterCard', props.data)
-  const [eachCharacter, setEachCharacter] = useState({});
-
-  useEffect(() => {
-    const id = props.data.id
-    console.log('id', id)
-
-    Axios
-        .get(`https://rickandmortyapi.com/api/character/${id}`)
-        .then(response =>{
-          console.log('response in CharacterCard', response)
-          setEachCharacter(response.data)
-        })
-        .catch(err => {
-          console.log('err in CharacterCard', err)
-        })
-  },[props.data.id]);
 
   return (
     <Card.Group itemsPerRow={2}>
     <Card>
-      <Image src={eachCharacter.image} alt="Image"></Image>
+      <Image src={props.data.image} alt="Image"></Image>
       <Card.Content>
-        <Card.Header>{eachCharacter.name}</Card.Header>
-        <Card.Meta>{eachCharacter.species} {eachCharacter.status}</Card.Meta>
+        <Card.Header>{props.data.name}</Card.Header>
+        <Card.Meta>{props.data.species} {props.data.status}</Card.Meta>
         {/* <p>Location: <LocationCard /> </p> */}
         <Card.Description>Origin: </Card.Description>
         {/* <p>Episode: <EpisodeCard data={eachCharacter.episode} /> </p> */}
